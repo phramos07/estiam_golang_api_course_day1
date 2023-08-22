8cf0fa75c55130f27922c0272854a69526efc112
Pedro Ramos <phramoscosta@gmail.com>
2023-08-22 10:19:02 -0300

    add 21

--- exercises/ex21.go
+++ exercises/ex21.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
+
 /*
 Exercise: Implementing a Plugin System
 
@@ -24,17 +30,80 @@ Test on main.
 
 // Plugin interface: defines the contract for every plugin.
 type Plugin interface {
-	Execute() string
+	Execute() (string, error)
 }
 
 // A printing plugin: will print a message
 
-type PrintPlugin struct{}
+type PrintPlugin struct {
+	message string
+}
+
+func (p *PrintPlugin) Execute() (string, error) {
+	return p.message, nil
+}
 
 // A math plugin: will perform a math operation (it needs 2 operands and 1 operator)
 
-type MathPlugin struct{}
+type MathPlugin struct {
+	op1      decimal.Decimal // you can use float64/32
+	op2      decimal.Decimal
+	operator string
+}
+
+func (m *MathPlugin) Execute() (string, error) {
+	var result decimal.Decimal
+	switch m.operator {
+	case "ADD":
+		result = m.op1.Add(m.op2)
+	case "DIV":
+		result = m.op1.Div(m.op2)
+	case "MUL":
+		result = m.op1.Mul(m.op2)
+	case "SUB":
+		result = m.op1.Sub(m.op2)
+	default:
+		return "", fmt.Errorf("Operator not recognized: %s", m.operator)
+	}
+
+	return fmt.Sprintf(
+		"%s %s %s = %s",
+		m.op1.String(),
+		m.operator,
+		m.op2.String(),
+		result.String(),
+	), nil
+}
+
+// Commander pattern
+type PluginCommander struct {
+	plugins []Plugin
+}
+
+func (p *PluginCommander) Execute() {
+	for _, plugin := range p.plugins {
+		response, err := plugin.Execute()
+		if err != nil {
+			fmt.Println("Error when executing plugin", err)
+			continue
+		}
+
+		fmt.Println(response)
+	}
+}
 
 func main() {
+	start := &PrintPlugin{message: "Starting plugins execution."}
+	mathPlugin := &MathPlugin{
+		op1:      decimal.NewFromFloat(3.5),
+		op2:      decimal.NewFromFloat(2.0),
+		operator: "MUL",
+	}
+	end := &PrintPlugin{message: "Plugins finished executing."}
 
+	commander := &PluginCommander{
+		plugins: []Plugin{start, mathPlugin, end},
+	}
+
+	commander.Execute()
 }
